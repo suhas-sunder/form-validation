d4be25a743f87ae72f628b1e297bcee1f49e83b6
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-09-26 23:38:33 -0400

    styling: email form
    -Add basic form elements. -Add styling to form elements using Tailwind CSS.

---
+++ src/form.tsx
@@ -0,0 +1,18 @@
+import { useState, useRef } from "react";
+
+function form() {
+  return (
+    <form className="flex flex-col items-center gap-10">
+      <div className="flex flex-col items-start text-2xl text-slate-600">
+        <label className="mb-3 pl-1">Email</label>
+        <input className="border border-slate-300 rounded p-2" />
+      </div>
+      <div className="flex flex-col items-start text-2xl text-slate-600">
+        <label className="mb-3 pl-1">Password</label>
+        <input className="border border-slate-300 rounded mb-8 p-2" />
+      </div>
+    </form>
+  );
+}
+
+export default form;
