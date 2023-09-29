1a1f4d2638785efe7458a492a76fb79db2258e4a
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-09-28 22:34:54 -0400

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Form from "./form";
+import Form from "./Form";
 import "./App.css";
 
 function App() {
--- src/form.tsx
+++ src/form.tsx
@@ -1,18 +1,42 @@
 import { useState, useRef } from "react";
 
-function form() {
+function Form() {
+  const [focused, setFocused] = useState<boolean>(false);
+  
+
+  const handleSubmission = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
   return (
-    <form className="flex flex-col items-center gap-10">
-      <div className="flex flex-col items-start text-2xl text-slate-600">
-        <label className="mb-3 pl-1">Email</label>
-        <input className="border border-slate-300 rounded p-2" />
+    <form
+      className="flex flex-col items-center gap-10"
+      onSubmit={handleSubmission}
+    >
+      <div className="flex flex-col items-start text-2xl text-slate-600 leading-tight">
+        <label htmlFor="email" className="mb-3 pl-1">
+          Email
+        </label>
+        <input
+          id="email"
+          placeholder="email"
+          className="border border-slate-300 rounded p-3 pt-2 pl-4"
+        />
       </div>
-      <div className="flex flex-col items-start text-2xl text-slate-600">
-        <label className="mb-3 pl-1">Password</label>
-        <input className="border border-slate-300 rounded mb-8 p-2" />
+      <div className="flex flex-col items-start text-2xl text-slate-600 leading-tight">
+        <label htmlFor="password" className="mb-3 pl-1">
+          Password
+        </label>
+        <input
+          id="password"
+          placeholder="password"
+          className="border border-slate-300 rounded mb-8 p-3 pt-2 pl-4 "
+        />
       </div>
+      <button>Login</button>
     </form>
   );
 }
 
-export default form;
+export default Form;
